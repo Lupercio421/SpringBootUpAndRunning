9cfe44bce6e03f9eb9564fb1c2d654a6d9116f99
Daniel L <daniel.llupercio@gmail.com>
2023-12-26 10:29:12 -0500

    textbook and repository code difference

--- java/planefinder/src/main/java/com/thehecklers/planefinder/PlaneRepository.java
+++ java/planefinder/src/main/java/com/thehecklers/planefinder/PlaneRepository.java
@@ -4,3 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PlaneRepository extends ReactiveCrudRepository<Aircraft, Long> {
 }
+//The textbook shows this as a <Aircraft, String>
