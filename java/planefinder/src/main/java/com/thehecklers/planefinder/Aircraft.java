ec7233f1464c69aa526a30b0d2eb52ed9cf27f1d
Daniel L <daniel.llupercio@gmail.com>
2024-01-01 17:44:49 -0500

    jakarta imports

--- java/planefinder/src/main/java/com/thehecklers/planefinder/Aircraft.java
+++ java/planefinder/src/main/java/com/thehecklers/planefinder/Aircraft.java
@@ -6,10 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.time.Instant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.time.Instant;
 
 @Data
 @NoArgsConstructor
